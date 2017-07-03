56efde0112d0fb4d496ea17d013451e6d68fd36e
Ajay Singh <ajaysbox@gmail.com>
2017-07-03 23:46:07 +0200

    recipe object to details comp..getting error to fix

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +6,7 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+@Output() recipeSelectEvent = new EventEmitter<Recipe>();
   recipes:Recipe[]=[
     new Recipe('Recipe name 1','Recipe description 1','https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg'),
     new Recipe('Recipe name 2','Recipe description 2','https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg'),
@@ -16,5 +17,8 @@ export class RecipesListComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  recipeSelected(item:Recipe){
+    this.recipeSelectEvent.emit(item);
+    console.log("recipe item selected = "+item.name+" description "+item.description)
+  }
 }
