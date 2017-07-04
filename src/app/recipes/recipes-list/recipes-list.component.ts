bdf5fa6f5cfbf94ae26aa058f22c4415f4af4b2a
Aj <ajaysbox@gmail.com>
2017-07-04 14:23:44 +0200

    to be overridden

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -7,14 +7,16 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipesListComponent implements OnInit {
   recipes:Recipe[]=[
-    new Recipe('A test1 recipe','this is a tasty recipe','https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg'),
-    new Recipe('A test2 recipe','this is a tasty recipe','https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg'),
-    new Recipe('A test3 recipe','this is a tasty recipe','https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg'),
-    new Recipe('A test4 recipe','this is a tasty recipe','https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg')
+    new Recipe('name 1 recipe','description recipe 1','https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg'),
+    new Recipe('name 2 recipe','description tasty recipe 2','https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg'),
+    new Recipe('name 3 recipe','description tasty recipe 3','https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg'),
+    new Recipe('name 4 recipe','description tasty recipe 4','https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-superJumbo.jpg')
   ];
   constructor() { }
 
   ngOnInit() {
   }
-
+recipeClicked(item:Recipe){
+  console.log("recipe clicked = "+item.description)
+}
 }
